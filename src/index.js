1e239e7ea7ec8edea42c109f7d3e33ca4f9c210e
L1feF0rm <30956836+L1feF0rm@users.noreply.github.com>
2022-07-15 01:16:21 +0300

    vue-router and vuex

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import {createApp} from "vue";
+import {createApp} from 'vue';
+import {createRouter, createWebHashHistory} from 'vue-router';
+import {createStore} from 'vuex';
 import App from "./App.vue";
 
-const app = createApp(App);
+const app = createApp(App)
+    .use(createRouter({
+        history: createWebHashHistory(),
+        routes: []
+    }))
+    .use(createStore({
+        namespaced: true,
+        state: {
+            hello: 'world'
+        }
+    }));
 app.mount("main#app");
+
+
+
+console.log('createRouter', createRouter);
+console.log('createWebHashHistory', createWebHashHistory);
+console.log('createStore', createStore);
